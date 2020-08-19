2771c5e02e284dc17305f7c303b314960658fd2c
Santiago Solana <santiago.sjs@gmail.com>
2020-08-19 14:17:42 -0400

    Built Employee class and other sub classes / All tests now pass!

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -4,6 +4,7 @@ test("Can set GitHUb account via constructor", () => {
   const testValue = "GitHubUser";
   const e = new Engineer("Foo", 1, "test@test.com", testValue);
   expect(e.github).toBe(testValue);
+  
 });
 
 test("getRole() should return \"Engineer\"", () => {
