694382d36a07cb3785c8e77164b33e470551e323
Santiago Solana <santiago.sjs@gmail.com>
2020-08-24 16:12:25 -0400

    Better Readme

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# Employee-Info-Manager
-This application uses inquirer to prompt the user for information on different employees through the command line. It then takes this information and creates an HTML page that displays your employees and their information.
+
+# Employee Information Manager     [![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
+
+This application uses inquirer to prompt the user for information on different employees through the command line. It then takes this information and creates an HTML page that displays your employees and their information. This app uses HTML, CSS, JavaScript, Node.js, Inquirer npm, Jest npm and ES6.
+    
+## Table of Contents
+    
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [Contributing](#contributing)
+4. [Test](#test)
+5. [Questions](#questions)
+6. [License](#license)
+    
+# Installation
+This application does not Require installation but does rely on Node.js to be installed. run the npm i command to install any dependencies and modules.
+# Usage
+ - On your terminal type node app.js to start 
+ - Fill in the information required about your Employees 
+ - Once finished your HTML page will populate in the output folder
+
+# Contributing
+There are no quidelines for contribution. Feel free to expand or refactor this application as you please.
+# Test
+This application uses jest for testing. type npm run test to start testing.
+# Questions
+If you have any questions or would like to contact me feel free to reach me at:
+- Email: santiago.sjs@gmail.com
+- Github: [santu14](https://github.com/santu14)
+
+## License
+
+                Copyright 2020  Santiago Solana
+
+    Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
--- app.js
+++ app.js
@@ -10,14 +10,193 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const allEmployees = [];
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+const initialQuestions = () => {
 
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email",
+            default: () => { },
+            validate: function (email) {
+
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+
+                if (valid) {
+                    return true;
+                } else {
+                    console.log(".  Please enter a valid email")
+                    return false;
+                }
+            }
+        },
+        {
+            type: "number",
+            name: "officeNumber",
+            message: "What is your office number?"
+        }
+    ]);
+}
+
+const engineerQuestions = () => {
+   return inquirer.prompt([
+        
+        {
+            type: "input",
+            name: "name",
+            message: "Enter your Engineers name: "
+        },
+        {
+            type: "number",
+            name: "id",
+            message: "Enter your Engineers ID number: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter your Engineers Email: ",
+            default: () => { },
+            validate: function (email) {
+
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+
+                if (valid) {
+                    return true;
+                } else {
+                    console.log(".  Please enter a valid email")
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Enter your Engineers GitHub user name: "
+        },
+        {
+            type: "confirm",
+            name: "addEngineer",
+            message: "Would you like to add another Engineer?"
+        },
+    ]);
+};
+
+const internQuestions = () => {
+    return inquirer.prompt([
+         
+         {
+             type: "input",
+             name: "name",
+             message: "Enter your Intern's name: "
+         },
+         {
+             type: "number",
+             name: "id",
+             message: "Enter your Intern's ID number: "
+         },
+         {
+             type: "input",
+             name: "email",
+             message: "Enter your Intern's Email: ",
+             default: () => { },
+             validate: function (email) {
+ 
+                 valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+ 
+                 if (valid) {
+                     return true;
+                 } else {
+                     console.log(".  Please enter a valid email")
+                     return false;
+                 }
+             }
+         },
+         {
+             type: "input",
+             name: "school",
+             message: "Enter your Intern's school: "
+         },
+         {
+             type: "confirm",
+             name: "addIntern",
+             message: "Would you like to add another Intern?"
+         },
+     ]);
+ };
+
+
+initialQuestions().then((answers) => {
+    const manager = new Manager(answers.name, 1, answers.email, answers.officeNumber);
+    allEmployees.push(manager);
+    console.log("----- ENGINEERS -----")
+
+    engineerQuestions().then((answers) => {
+        saveEngineer(answers);
+
+    });
+});
+
+
+
+
+const saveEngineer = (answers) => {
+    // const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+    allEmployees.push(new Engineer(answers.name, answers.id, answers.email, answers.github))
+    // console.log(engineer);
+    if(answers.addEngineer){
+        engineerQuestions().then((answers) => {
+            saveEngineer(answers);
+        });
+    } else {
+        // console.log(allEmployees);
+        console.log("----- INTERS -----");
+
+        internQuestions().then((answers) => {
+            saveIntern(answers);
+            
+        });
+    }
+};
+
+
+
+
+
+const saveIntern = (answers) => {
+    
+    allEmployees.push(new Intern(answers.name, answers.id, answers.email, answers.school))
+   
+    if(answers.addIntern){
+        internQuestions().then((answers) => {
+            saveIntern(answers);
+        });
+    } else {
+        // console.log(allEmployees);
+        fs.writeFile(outputPath, render(allEmployees), function(err) {
+
+            if (err) {
+              return console.log(err);
+            }
+          
+            console.log("Success! your page has been created!");
+          
+          });
+        return
+    }
+};
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
